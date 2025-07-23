a8a7517997b6186232f3e780b97ecf76bad38ce7
brandongarnica <brandonsgithubcode@gmail.com>
2025-07-23 12:38:26 -0600

    Feat: initialize and tested SDL window and rendered a color in main.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #define SDL_MAIN_HANDLED
 
-#include <SDL.h>
+#include <SDL3/SDL.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
@@ -9,20 +9,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "Minesweeper", 
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        640, 480, 
-        SDL_WINDOW_SHOWN
-    );
-
+    SDL_Window* window = SDL_CreateWindow("Minesweeper", 640, 480, SDL_WINDOW_OPENGL);
     if (!window) {
         fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL); // NULL = default renderer
     if (!renderer) {
         fprintf(stderr, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
@@ -30,7 +24,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Blue
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
@@ -38,7 +32,7 @@ int main(int argc, char* argv[]) {
     int quit = 0;
     while (!quit) {
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+            if (e.type == SDL_EVENT_QUIT) {
                 quit = 1;
             }
         }
